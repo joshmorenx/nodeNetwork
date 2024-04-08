ba1805be9a53c8909187dbf029e5b37b34fdae2c
joshmorenx <joshuamoreno21@gmail.com>
2024-04-08 16:35:43 -0600

    Coding the function to gather the selected user permissions

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -32,56 +32,8 @@ app.use(
 );
 
 app.post("/", login);
-// Cerrar sesion
-
 app.post("/logout", logout);
-
-// Ruta de registro
-// app.get("/register", (req, res) => {
-//     res.render("register");
-// });
-
-// app.get("/register", register);
 app.post("/register", register);
-
-// app.post("/xd", async (req, res) => {
-//     const { firstName, lastName, email, username, password, pwdConfirmation } = req.body;
-//     const existingUser = await User.findOne({ username });
-//     const existingEmail = await User.findOne({ email });
-//     // const existingRegistryNumber = await User.findOne({ "organization.registryNumber": registryNumber });
-
-//     errExisting = (err) => {
-//         return res.render("register", { error: err });
-//     }
-
-//     if ( existingUser && existingEmail ) {
-//         errExisting("El usuario, el correo y la matrícula ya existen")
-//     } else if ( existingUser || existingEmail ) {
-//         errExisting('El usuario, el correo o la matrícula ya existen')
-//     } else {
-//         const hashedPassword = await bcrypt.hash(password, 10);
-//         // Obtener el último ID registrado
-//         const lastUser = await User.findOne({}, {}, { sort: { id: -1 } });
-
-//         // Calcular el nuevo ID
-//         const newId = lastUser ? lastUser.id + 1 : 1;
-
-//         // Crear el nuevo usuario con el ID autoincremental
-//         const user = new User({
-//             id: newId,
-//             firstName,
-//             lastName,
-//             email,
-//             username,
-//             password: hashedPassword,
-//         });
-
-//         await user.save();
-//         res.redirect("/");
-//     }
-
-// });
-
 app.get('/api/usuario', verificarToken, (req, res) => {
     const usuario = req.usuario;
 
@@ -90,11 +42,6 @@ app.get('/api/usuario', verificarToken, (req, res) => {
 
     res.json({ userId, username, firstName, lastName, email, permissions });
 });
-
-// app.get('/api/usuarios', async (req, res) => {
-//     const allUsernames = await User.find({}, { username: 1, _id: 0 });
-//     return res.status(200).json({usernames : allUsernames});
-// })
 
 app.get("/api/usuarios", getAllUsers)
 
