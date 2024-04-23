a9660bec7c88b2e44846b85f56ae40e15552232d
joshmorenx <joshuamoreno21@gmail.com>
2024-04-23 14:01:55 -0600

    Cleaning pollution in code

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -18,16 +18,13 @@ const Login = () => {
     useEffect(() => {
         try {
             if (tokenState) {
-                Cookies.set('token', tokenState, { expires: 7, secure: true, sameSite: 'Strict' });
-                setTimeout(() => {
-                    // useSetExpiryTimeServerSide(tokenState); just to make sure that i will develop this in the future
-                    window.location.reload();
-                }, 1000 + 3000 * Math.random());
+                Cookies.set('token', tokenState, { expires: 7, secure: true, sameSite: 'Strict' }); // useSetExpiryTimeServerSide(tokenState); just to make sure that i will develop this in the future
+                window.location.reload();
             }
         } catch (error) {
             console.error(error);
         }
-    }, [tokenState]);
+    }, [tokenState, loginData, userInfo]);
 
     return (
         <LoginDisplayer handleInputChange={handleInputChange} formData={formData} sendForm={sendForm} userInfo={userInfo} open={open} preHandleClose={preHandleClose} loginData={loginData} />  
