245e26380ad546d9413dbd4145538de390ffe9d1
joshmorenx <joshuamoreno21@gmail.com>
2024-04-30 16:32:37 -0600

    Accordions added to permission creation/deletion for better UX

--- client/src/components/PermissionCreateOrDelete.jsx
+++ client/src/components/PermissionCreateOrDelete.jsx
@@ -1,13 +1,59 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl'
+import FormLabel from '@mui/material/FormLabel'
+import FormHelperText from '@mui/material/FormHelperText'
+
 
 export default function PermissionCreateOrDelete({ ListaPermisos, permissionDetails, handleSelectedChange, PermissionAddOrDel, token, selectedPermission }) {
     return (
         <>
-            <div>
-                <ListaPermisos permissionDetails={ permissionDetails } handleSelectedChange={ handleSelectedChange } />
-            </div>
-            <div>
-                <PermissionAddOrDel token={ token } selectedPermission={ selectedPermission } permissionDetails={ permissionDetails } />
-            </div> 
+        <Accordion color='primary'>
+            <AccordionSummary
+            expandIcon={<ArrowDropDownIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+            sx={{ mt: 2, bgcolor: '#faf1de' }}
+            >
+            <Typography>Eliminar Permisos</Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={{ bgcolor: '#faf1de' }}>
+            <Box>
+                <Box>
+                    <ListaPermisos permissionDetails={ permissionDetails } handleSelectedChange={ handleSelectedChange } />
+                </Box>
+                <Box>
+                    <PermissionAddOrDel token={ token } selectedPermission={ selectedPermission } permissionDetails={ permissionDetails } />
+                </Box> 
+            </Box>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion color='primary'>
+            <AccordionSummary
+            expandIcon={<ArrowDropDownIcon />}
+            aria-controls="panel2-content"
+            id="panel2-header"
+            sx={{ mt: 2, bgcolor: '#faf1de' }}
+            >
+            <Typography>Crear Permisos</Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={{ bgcolor: '#faf1de' }}>
+            <Box>
+                <FormControl>
+                    <FormLabel></FormLabel>
+                    <Typography variant="body1" color="initial">WORK IN PROGRESS</Typography>
+                    <FormHelperText></FormHelperText>
+                </FormControl>
+            </Box>
+            </AccordionDetails>
+        </Accordion>
+        
         </>   
     )  
 }
