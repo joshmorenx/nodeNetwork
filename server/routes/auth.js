dd56cb62d1aabfd526ee890d612220751278e2c5
joshmorenx <joshuamoreno21@gmail.com>
2024-05-02 00:03:43 -0600

    Permission creation/deletion API, now working, working on the frontend to refresh ListaPermisos to avoid any errors when deleting deleted permission

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -54,42 +54,43 @@ app.post("/api/update_permissions/", updatePermissionsForOneUser)
 
 app.get('/api/getAllPermissions/', getAllPermissions);
 app.post('/api/modifyPermissions/', updatePermissions);
-app.get("/api/getPermissionDescription", getPermissionDescription)
+app.get("/api/getPermissionDescription/", getPermissionDescription)
 
 app.post('/api/lastPermission/',async (req, res) => {
-    const { typeUpdate, permId, permName } = req.body
+    const { typeUpdate, permId, permName, newPermName, newPermDesc } = req.body
+    // const lastPermissionRegistry = await Permission.findOne({},{permissionId:1},{sort:{ permissionId: -1 }});
     
-    
-    const usersThatUseThisPermission = []
-    // check if that permission is being used by another user if not delete it
-
-    const perm_Id = await Permission.findOne({ permissionId: permId });
-    const filteredUser = await User.find({ permissions: perm_Id._id });
-
-    const lastPermissionRegistry = await Permission.findOne({},{permissionId:1},{sort:{ permissionId: -1 }});
-    
-    filteredUser.map((user) => {
-        usersThatUseThisPermission.push(user.username)
-    })
-
     try {
-        const lastPermissionRegistry = await Permission.findOne({},{permissionId:1},{sort:{ permissionId: -1 }});
         if(typeUpdate === 'add') {
-            const newPermission = new Permission({
-                permissionId: lastPermissionRegistry.permissionId + 1,
-                permissionName: permName,
-                permissionDescription: permDescription
-            })
-        
-            const result = await newPermission.save();
+            const lastPermissionRegistry = await Permission.findOne({},{permissionId:1},{sort:{ permissionId: -1 }});
+            if(newPermName && newPermDesc){
+                const newPermission = new Permission({
+                    permissionId: lastPermissionRegistry.permissionId + 1,
+                    permissionName: newPermName,
+                    permissionDescription: newPermDesc
+                })
+            
+                const result = await newPermission.save();
 
-        if(result) {
-            res.json({message:"Permiso creado correctamente"});
-        }
-
+                if(result) {
+                    res.json({message:"Permiso creado correctamente"});
+                }
+            } else {
+                res.json({message:"Faltan datos por llenar"});
+            }
         }
 
         else if(typeUpdate === 'remove') {
+            
+            const usersThatUseThisPermission = []
+            // check if that permission is being used by another user if not delete it
+            const perm_Id = await Permission.findOne({ permissionId: permId });
+            const filteredUser = await User.find({ permissions: perm_Id._id });
+
+            filteredUser.map((user) => {
+                usersThatUseThisPermission.push(user.username)
+            })
+
             if(usersThatUseThisPermission.length > 0) {
                 res.json({message:"No se puede borrar el permiso ya que esta siendo usado por otro usuario.", usersThatUseThisPermission: usersThatUseThisPermission});
             } else {
