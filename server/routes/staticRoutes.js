77ecb2a77972610963121827f30be6659258039d
joshmorenx <joshuamoreno21@gmail.com>
2024-05-14 23:41:33 -0600

    Uncommented the method get for static files in order to set a middleware for the images not to be public at all

--- server/routes/staticRoutes.js
+++ server/routes/staticRoutes.js
@@ -3,18 +3,19 @@ const app = express()
 const path = require('path');
 
 // static files
-app.use('/api/public/', express.static(path.join(__dirname, '../public')))
+// app.use('/api/public/', express.static(path.join(__dirname, '../public')))  // the ez way
 
 // get static profile user picture
 // server\public\uploads\users\joshmorenx\profile\profile.jpg
-// app.get('/api/public/uploads/users/:username/profile/profile.jpg', (req, res) => {
-//     const { username } = req.params
-//     try {
-//         res.sendFile(path.resolve(__dirname, '../public/uploads/users', username, 'profile', 'profile.jpg'))
-//     } catch (error) {
-//         res.status(500).send('Error al obtener la imagen de perfil del usuario')
-//     }
-// })
+
+app.get('/api/public/uploads/users/:username/profile/profile.jpg', /*middleware,*/ (req, res) => { // best way -> in order to set a middleware
+    const { username } = req.params
+    try {
+        res.sendFile(path.resolve(__dirname, '../public/uploads/users', username, 'profile', 'profile.jpg'))
+    } catch (error) {
+        res.status(500).send('Error al obtener la imagen de perfil del usuario')
+    }
+})
 
 
 module.exports = app
