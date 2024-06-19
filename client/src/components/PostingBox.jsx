bf702efc44b94311550b8c8f8549463fd9a0d933
joshmorenx <joshuamoreno21@gmail.com>
2024-06-19 00:55:29 -0600

    adding profile viewer

--- client/src/components/PostingBox.jsx
+++ client/src/components/PostingBox.jsx
@@ -19,7 +19,7 @@ export default function PostingBox({ token, handleFeedReload }) {
             <>  
                 {showPostingBox && <PopUpPostingBox token={token} handleClosePostingBoxPopUp={handleClosePostingBoxPopUp} handleFeedReload={handleFeedReload} />}
                 
-                <Box sx={{ margin : '20px', display: 'flex'}}>
+                    <Box sx={{ margin : '20px', display: 'flex', bgcolor: 'wheat'}}>
                     <TextField
                     size="small"
                     placeholder="Escribe lo que piensas..."
--- client/src/components/ProfileDisplayer.jsx
+++ client/src/components/ProfileDisplayer.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import useGetSpecificUserData from "../hooks/useGetSpecificUserData.jsx";
 import { Box, Typography } from "@mui/material";
 import UserCard from "./UserCard.jsx";
 import usePermissions from "../hooks/usePermissions.jsx";
+import ImageGallery from "./ImageGallery.jsx";
+import FeedContent from "./FeedContent.jsx";
 
 
 export default function ProfileDisplayer({ token, username }) {
@@ -14,25 +16,26 @@ export default function ProfileDisplayer({ token, username }) {
     }, [])
 
     return (
-        <Box className="wrapper">
-            <Box>
-                <UserCard user={userData} allAccess={allAccess} cadena={cadena} />
+        <>
+            <Typography variant="h2" component="h2">
+                ES NECESARIO MOSTRAR EL DEL USUARIO EN EL PARAMETRO DE LA URL Y NO LA DEL USUARIO ACTUAL HACER EL AJUSTE NECESARIO
+            </Typography>
+            <Box sx={{ position: 'fixed', backgroundColor: 'rgb(125, 106, 155)', width: '100%', height: '100%', zIndex: -1 }}></Box>
+            <Box sx={{ width: '50%', justifyContent: 'left', alignItems: 'left', display: 'flex', ml: 10 }}>
+                <Box className="fixed-profile">
+                    <Box sx={{ ml: 0.5, mt: 2, width: '300px' }}>
+                        <UserCard user={userData} allAccess={allAccess} cadena={cadena} />
+                    </Box>
+                    <Box sx={{ ml: 2, mt: 2, mr: 2, width: '91%', height: '40%' }}>
+                        <ImageGallery token={token} username={username} />
+                    </Box>
+                </Box>
+                <Box className="wrapper">
+                    <Box className='one'>
+                        <FeedContent token={token} />
+                    </Box>
+                </Box>
             </Box>
-            <Box>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-            </Box>
-            <Box>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-            </Box>
-            <Box>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-                <Typography variant="h2" color="initial"> grid demo </Typography>
-            </Box>
-        </Box>
+        </>
     )
 }
