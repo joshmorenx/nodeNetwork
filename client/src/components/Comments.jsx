81e06ea8245512954efbfa7f94d1aa4c9a73657a
joshmorenx <joshuamoreno21@gmail.com>
2024-07-01 17:49:32 -0600

    working on likes/dislikes on comments

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,7 +1,9 @@
-import { Box, Avatar, Typography } from '@mui/material'
+import { Box, Button, Avatar, Typography, Stack } from '@mui/material'
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 
 export default function Comments({ comment }) {
     const [formattedDate, setFormattedDate] = useState('');
@@ -14,14 +16,22 @@ export default function Comments({ comment }) {
 
     return (
         <Box sx={{ alignItems: 'center', border: '1px solid grey', margin: '8px', padding: '8px' }}>
-                <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-                    <Avatar>{comment.username.charAt(0).toUpperCase()}</Avatar> 
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                <Avatar>{comment.username.charAt(0).toUpperCase()}</Avatar>
                 <Typography> {comment.username} </Typography>
                 <Typography sx={{ ml: '10px', border: '1px solid grey', padding: '5px', color: 'white', bgcolor: 'black' }}> creado el {formattedDate} </Typography>
             </Box>
             <Box sx={{ alignItems: 'center', border: '1px solid grey', margin: '8px', padding: '8px' }}>
                 <Typography variant="body2"> {comment.content} </Typography>
             </Box>
+            <Stack direction="row" sx={{ display: 'flex' }}>
+                <Button>
+                    <ThumbUpIcon /> <span style={{ marginLeft: '5px' }}> Me Gusta (1) </span>
+                </Button>
+                <Button>
+                    <ThumbDownIcon color="error" /> <span style={{ marginLeft: '5px' }}> No Me Gusta (1) </span>
+                </Button>
+            </Stack>
         </Box>
     )
 }
