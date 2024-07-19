98c93635f44abff06517477c0c90e707a0410b9e
joshmorenx <joshuamoreno21@gmail.com>
2024-07-18 23:17:36 -0600

    post isolation working as intented when using the route /posts/<post_id>

--- client/src/routes/Posts.jsx
+++ client/src/routes/Posts.jsx
@@ -1,30 +1,36 @@
 import FeedNavbar from "../components/FeedNavbar.jsx";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Typography } from "@mui/material";
 import useGetCurrentUser from "../hooks/useGetCurrentUser.jsx";
 import useGetSpecificUserData from "../hooks/useGetSpecificUserData.jsx";
 import { Box } from "@mui/material";
 import useGetSinglePost from "../hooks/useGetSinglePost.jsx";
-
+import PostedContent from "../components/PostedContent.jsx";
 export default function Posts({ token }){
     const { post_id } = useParams();
     const { post, error, success, msg, getSinglePost } = useGetSinglePost({ token, id: post_id });
+    const navigate = useNavigate();
 
     useEffect(()=>{
-        if(post_id) getSinglePost();
+        if(post_id > 0){
+            getSinglePost()
+        } else {
+            navigate('/feed')
+        }
     }, [post_id])
 
     return(
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',  }}>
-            <FeedNavbar token={token} />
-            <Box>Posts page</Box>
-            {post_id && <Box>{post_id}</Box>}
-            {post && <Box>{post.content}</Box>}
-            {error && <Typography sx={{ color: 'red' }}>
-                No se ha encontrado el post
-            </Typography>}
-        </Box>
-        
+        <>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <FeedNavbar token={token} />
+                <Box sx={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: -1, bgcolor: '#220040' }}></Box>
+                <Box sx={{ width: '50vw' }}>
+                    {success && <PostedContent token={token} post={post} />}
+                </Box>
+                {error && <Typography sx={{ color: 'red', stroke: 'red', mt: '20px' }} variant="h3">No se ha encontrado el post</Typography>}
+            </Box>
+        </>        
     )
 }
