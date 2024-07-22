7bbb684b45624ce4ddfb02ef255fa11a93ba8e8b
joshmorenx <joshuamoreno21@gmail.com>
2024-07-22 11:53:21 -0600

    adding the route of the followers/following users of any user

--- client/src/routes/Feed.jsx
+++ client/src/routes/Feed.jsx
@@ -1,13 +1,17 @@
 import Navbar from '../components/Navbar';
 import FeedContent from '../components/FeedContent';
 import PropTypes from 'prop-types'
-import { Avatar, Box, Typography, Link } from '@mui/material';
+import { Avatar, Box, Typography, Link, Stack } from '@mui/material';
 import useGetAllUsers from '../hooks/useGetAllUsers';
 import { useEffect, useState } from 'react';
+import useGetCurrentUser from '../hooks/useGetCurrentUser';
+import ThreePIcon from '@mui/icons-material/ThreeP';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
 
 export default function Feed({ token }) {
     const { userNames } = useGetAllUsers();
     const [nombres, setNombres] = useState([]);
+    const { user } = useGetCurrentUser({ token });
     const usernameLinkStyles = [
         { mt: '10px', ml: '10px', display: 'flex', justifyContent: 'left', alignItems: 'center', textDecoration: 'none', cursor: 'pointer', ":hover": { textDecoration: 'underline' } },
         { color: 'white', mt: '10px', ml: '10px' }
@@ -41,8 +45,33 @@ export default function Feed({ token }) {
 
                         {/* Contenedor para los elementos fijos (Eventos) */}
                         <Box sx={{ position: 'fixed', top: '73px', left: '0', width: '20%', height: '100%' }}>
-                            <Box className='bg-pink-500' style={{ width: '100%', height: '100%', marginBottom: '10px' }}>
-                                <p>Eventos</p>
+                            <Box className='bg-red-500' style={{ width: '100%', height: '100%', marginBottom: '10px' }}>
+                                <Box sx={{ display: 'flex', justifyContent: 'justify', alignItems: 'center', pt: '10px' }}>
+                                    <Stack direction="row" spacing={2}>
+                                        {user.username ? (
+                                            <Link href={`/profile/${user.username}`} sx={usernameLinkStyles[0]}>
+                                                <Avatar> {user.username.charAt(0).toUpperCase()}</Avatar>
+                                                <Typography sx={usernameLinkStyles[1]}>{user.username}</Typography>
+                                            </Link>
+                                        ) : (<></>)}
+                                    </Stack>
+                                </Box>
+                                <Box sx={{ display: 'flex', justifyContent: 'justify', alignItems: 'center', pt: '10px' }}>
+                                    <Stack direction="row" spacing={2}>
+                                        <Link href={"#"} sx={usernameLinkStyles[0]}>
+                                            <ThreePIcon sx={usernameLinkStyles[1]} />
+                                            <Typography sx={usernameLinkStyles[1]}>Seguidores</Typography>
+                                        </Link>
+                                    </Stack>
+                                </Box>
+                                <Box sx={{ display: 'flex', justifyContent: 'justify', alignItems: 'center', pt: '10px' }}>
+                                    <Stack direction="row" spacing={2}>
+                                        <Link href={"#"} sx={usernameLinkStyles[0]}>
+                                            <HowToRegIcon sx={usernameLinkStyles[1]} />
+                                            <Typography sx={usernameLinkStyles[1]}>Usuarios seguidos</Typography>
+                                        </Link>
+                                    </Stack>
+                                </Box>
                             </Box>
                         </Box>
 
@@ -51,7 +80,7 @@ export default function Feed({ token }) {
                             <FeedContent token={token} />
                         </Box>
 
-                        {/* Contenedor para los elementos fijos (Trends) */}
+                        {/* Contenedor para los elementos fijos (usuarios mas recientes) */}
                         <Box sx={{ position: 'fixed', top: '73px', right: '0', width: '20%', height: '100%' }}>
                             <Box className='bg-red-500' style={{ width: '100%', height: '100%', marginBottom: '10px' }}>
                                 <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -60,9 +89,9 @@ export default function Feed({ token }) {
                                 {
                                     nombres.map((nombre, index) => {
                                         return (
-                                        <Link href={`/profile/${nombre}/`} key={index} sx={usernameLinkStyles[0]}>
-                                            <Avatar>{nombre.charAt(0)}</Avatar><Typography sx={usernameLinkStyles[1]}>{nombre}</Typography>
-                                        </Link>
+                                            <Link href={`/profile/${nombre}/`} key={index} sx={usernameLinkStyles[0]}>
+                                                <Avatar>{nombre.charAt(0)}</Avatar><Typography sx={usernameLinkStyles[1]}>{nombre}</Typography>
+                                            </Link>
                                         )
                                     })
                                 }
