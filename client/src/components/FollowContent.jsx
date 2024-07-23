bc0017db79cd44e99a9a22e806d100ae2fb3fc13
joshmorenx <joshuamoreno21@gmail.com>
2024-07-23 16:11:24 -0600

    still working on follows route

---
+++ client/src/components/FollowContent.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Avatar, Button, Stack, Box, Typography } from '@mui/material';
+import useFollowUser from '../hooks/useFollowUser';
+import useUnfollowUser from '../hooks/useUnfollowUser';
+import useGetCurrentUser from '../hooks/useGetCurrentUser';
+
+export default function FollowContent({ token, username }) {
+    const { user, error } = useGetCurrentUser({ token });
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Typography>We are working on this section. Please come back later {user.username ? user.username : ''}</Typography>
+            <Box>
+                <Box sx={{ display: 'inline-flex', alignItems: 'center', gap: '100%' }} className="follows">
+                    <Box>
+                        <Typography sx={{ display: 'inline-flex', alignItems: 'center', gap: '10px' }} >
+                            <Avatar>H</Avatar>
+                            <span>{username}</span>
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Button variant="contained">Seguir</Button>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
--- client/src/routes/Follows.jsx
+++ client/src/routes/Follows.jsx
@@ -2,13 +2,15 @@ import Navbar from "../components/Navbar";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Box } from "@mui/material";
+import FollowContent from "../components/FollowContent";
 // import useGetUserFollows from "../hooks/useGetUserFollows";
 
 export default function Follows({ token }) {
+    const { username } = useParams()
     return(
         <>
-            <Navbar />
-            {"We are working on this section. Please come back later"}
+            <Navbar token={token} />
+            <FollowContent token={token} username={username} />
         </>
     )
 }
