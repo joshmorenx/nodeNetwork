9fb0ebdf03fa9b03fd2a4ffa2df9272174a113f7
joshmorenx <joshuamoreno21@gmail.com>
2024-07-26 16:32:21 -0600

    propper distance between following/follower users and unfollow button, still working on the styling

--- client/src/components/DisplayedContent.jsx
+++ client/src/components/DisplayedContent.jsx
@@ -4,16 +4,16 @@ import { Avatar, Button, Stack, Box, Typography } from '@mui/material';
 export default function DisplayedContent({ username, following, followers, selectedTab }) {
     return (
         <>
-            <Typography> Selecciona una pestaña para ver sus contenidos. </Typography>
+            { selectedTab == 0 && <Typography> Selecciona una pestaña para ver sus contenidos. </Typography> }
 
-            <Box sx={{ display: 'flex', gap: '10px' }}>
+            <Box sx={{ display: 'flex', alignContent: 'center', justifyContent: 'center', gap: '10px' }}>
 
                 {selectedTab == 1 &&
-                    <Box sx={{ display: 'block', alignItems: 'center', gap: '10px', border: '1px solid black' }}>
+                    <Box sx={{ display: 'block', alignItems: 'center', gap: '10px' }}>
                         <Typography>Seguidores</Typography>
                         {followers.map((follower, key) => (
                             <Box key={key} sx={{ gap: '10px' }}>
-                                <Box sx={{ display: 'flex', gap: '10px', mb: '10px' }}>
+                                <Box sx={{ display: 'flex', gap: '50vw', mb: '10px', border: '1px solid black', p: '10px', borderRadius: '10px' }}>
                                     <Box>{follower.username}</Box>
                                     <Button variant="contained">Quitar de seguidores</Button>
                                 </Box>
@@ -23,11 +23,11 @@ export default function DisplayedContent({ username, following, followers, selec
                 }
 
                 {selectedTab == 2 &&
-                    <Box sx={{ display: 'block', alignItems: 'center', gap: '10px', border: '1px solid black' }}>
+                    <Box sx={{ display: 'block', alignItems: 'center', gap: '10px' }}>
                         <Typography>Siguiendo</Typography>
                         {following.map((following, key) => (
                             <Box key={key} sx={{ gap: '10px' }}>
-                                <Box sx={{ display: 'flex', gap: '10px', mb: '10px' }}>
+                                <Box sx={{ display: 'flex', gap: '50vw', mb: '10px', border: '1px solid black', p: '10px', borderRadius: '10px' }}>
                                     <Box>{following.username}</Box>
                                     <Button variant="contained">Quitar de seguidores</Button>
                                 </Box>
