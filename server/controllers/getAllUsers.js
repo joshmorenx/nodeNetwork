cbc8406b64067c8c396bafde77e1c4b9fb3bbfb9
joshmorenx <joshuamoreno21@gmail.com>
2024-08-01 17:20:24 -0600

    fix to avoid pasing the encrypted password in getAllUsers backend control

--- server/controllers/getAllUsers.js
+++ server/controllers/getAllUsers.js
@@ -4,7 +4,7 @@ const User = require("../models/User.js");
 const getAllUsers = async (req = request, res = response) => {
     // const allUsernames = await User.find({}, { username: 1, _id: 0 });
     try {
-        const allUsernames = await User.find({}).lean();
+        const allUsernames = await User.find({},{password:0}).lean();
         return res.status(200).json({usernames : allUsernames});
         // req.usernames = allUsernames
     } catch (error) {
