b31d1f2a5b448da12d977e549350aa0000391d56
joshmorenx <joshuamoreno21@gmail.com>
2024-09-04 17:43:15 -0600

    Added notifications created when comment a post

--- server/controllers/addComment.js
+++ server/controllers/addComment.js
@@ -1,12 +1,13 @@
 const User = require("../models/User.js");
 const Posts = require("../models/Posts.js");
 const Comments = require("../models/Comments.js");
+const Notifications = require("../models/Notifications.js");
 
 const addComment = async (req, res) => {
-    
+
     const { username } = req.usuario;
-    const { postId, content } = req.body;	
-    
+    const { postId, content } = req.body;
+
     try {
         const user = await User.findOne({ username: username }).lean();
         const post = await Posts.findOne({ postId: postId }).lean();
@@ -14,17 +15,26 @@ const addComment = async (req, res) => {
 
         const result = await Comments.create({ commentId: lastComment.commentId + 1, postId: post._id, author: user._id, content: content });
 
-        if(result){
+        if (result) {
+            
+            const latestNotification = await Notifications.findOne({}, {}, { sort: { notificationId: -1 } }).lean()
+
+            if (user._id.equals(post.author) === false) {
+                await Notifications.create({ from: user._id, reason: "comment", to: post.author, postId: post._id, notificationId: latestNotification === null ? 1 : latestNotification.notificationId + 1, reason: "comment", description: `${user.username} comentó tu publicacion` });
+            }
+            
             const newCurrentComments = await Comments.find({ postId: post._id }).lean();
-            res.json({ success: true, message: "Comentario creado correctamente", newCurrentComments: await Promise.all(newCurrentComments.map(async (comment) => {
-                const user = await User.findOne({ _id: comment.author }).lean();
-                return {
-                    ...comment,
-                    username: user.username,
-                    firstName: user.firstName,
-                    lastName: user.lastName
-                }
-            }))});
+            res.json({
+                success: true, message: "Comentario creado correctamente", newCurrentComments: await Promise.all(newCurrentComments.map(async (comment) => {
+                    const user = await User.findOne({ _id: comment.author }).lean();
+                    return {
+                        ...comment,
+                        username: user.username,
+                        firstName: user.firstName,
+                        lastName: user.lastName
+                    }
+                }))
+            });
         }
     } catch (error) {
         console.log(error);
