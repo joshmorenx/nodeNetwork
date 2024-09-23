0e0d11856486cbf62413275d335ff075163c4bf8
joshmorenx <joshuamoreno21@gmail.com>
2024-09-23 16:07:29 -0600

    Fixing the way the gallery route gathers the gallery images. Fixes #24

--- client/src/components/ImageGallery.jsx
+++ client/src/components/ImageGallery.jsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageViewer from './ImageViewer.jsx';
+import React, { useEffect } from 'react';
+import useGetUserGallery from '../hooks/useGetUserGallery'; // Ajusta la ruta según tu estructura de carpetas
 import { Box } from '@mui/material';
-import useGetGalleryImage from '../hooks/useGetGalleryImage';
+import ImageViewer from './ImageViewer';
+import { useState } from 'react';
 
-export default function ImageGallery({ token, username, userData }) {
-  const images = []
-  const [userImages, setUserImages] = useState([])
+const ImageGallery = ({ token, username }) => {
+  const { userGallery, isLoading, error } = useGetUserGallery({ token, username });
   const [imgClickedPath, setImgClickedPath] = useState(null)
-  const { galleryImage, imageError, getGalleryImage } = useGetGalleryImage({ username: username })
+
+  // Puedes mostrar un mensaje de carga o un error si es necesario
+  if (isLoading) return <div>Cargando...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   const handleImageClicked = (event) => {
     if (event) {
@@ -18,34 +18,24 @@ export default function ImageGallery({ token, username, userData }) {
     }
   }
 
-  useEffect(() => {
-    if (username && userData.galleryPictures !== undefined) {
-      userData.galleryPictures.forEach((item) => {
-        getGalleryImage(item.slice(item.indexOf('gallery') + 'gallery/'.length))
-      })
-    }
-  }, [username, userData.galleryPictures]);
-
-  useEffect(() => {
-    if (galleryImage) {
-      images.push(galleryImage)
-    }
-  }, [galleryImage])
-
-  useEffect(() => {
-    if (images.length > 0) {
-      setUserImages(images)
-    }
-  }, [images])
-
   return (
-    <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(164px, 1fr))', gap: '10px', }} >
-      {userImages !== undefined && (
-        userImages.map((elem, key) => (
-          <img onClick={(event) => { handleImageClicked(event) }} key={key} src={elem} alt="imagen alternativa" onError={(e) => e.target.src = "https://via.placeholder.com/200x200/ffffff/000000?text=Imagen+No+Disponible&size=30"} loading="lazy" style={{ width: '100%', height: 'auto' }} />
-        ))
-      )}
+    <>
+      <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(164px, 1fr))', gap: '10px' }}>
+        {userGallery.map((image, index) => (
+          <img
+            onClick={(event) => handleImageClicked(event)}
+            key={index}
+            src={image}
+            alt={`Imagen ${index + 1}`}
+            onError={(e) => (e.target.src = "https://via.placeholder.com/200x200/ffffff/000000?text=Imagen+No+Disponible")}
+            loading="lazy"
+            style={{ width: '100%', height: 'auto' }}
+          />
+        ))}
+      </Box>
       <ImageViewer image={imgClickedPath} setImgClickedPath={setImgClickedPath} />
-    </Box>
+    </>
   );
-}
+};
+
+export default ImageGallery;
