9273d2cf321eaf1c2b40e9176d8e2aa7caf541c1
Joshua Emmanuel Aranda Moreno <jaranda@bancodonde.com>
2026-02-23 15:31:48 -0600

    Adjustments for clean code practices and tsconfig.json in order to start migrating from jsx to tsx

--- client/src/components/LoginDisplayer.jsx
+++ client/src/components/LoginDisplayer.jsx
@@ -12,103 +12,76 @@ export default function LoginDisplayer({ handleInputChange, formData, sendForm, 
     const [gatheredLoginData, setGatheredLoginData] = useState('');
     const isDesktop = useMediaQuery('(min-width: 900px)');
     const isTablet = useMediaQuery('(min-width: 426px) and (max-width: 899px)');
-    const isMobile = useMediaQuery('(max-width: 425px)');
-
-    const desktopStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        width: '100vw',
-        backgroundColor: 'black',
-    }
-
-    const mobileStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        width: '100vw',
-        backgroundColor: 'black',
-    }
 
     useEffect(() => {
         loginData ? setGatheredLoginData(loginData) : setGatheredLoginData('Por favor, Espere...');
     }, [loginData])
 
     return (
-        <>
-            <Box className='login-background'>
-                <Box className={`${isDesktop ? 'login-container' : (isTablet ? 'login-container-tablet' : 'login-container-mobile')} fadeIn`}>
-                    <h1 className="login-title">Iniciar sesión</h1>
-                    <form className={isDesktop ? 'login-form' : (isTablet ? 'login-form-tablet' : 'login-form-mobile')} method="post" action="/">
+        <Box className='login-background'>
+            <Box className={`${isDesktop ? 'login-container' : (isTablet ? 'login-container-tablet' : 'login-container-mobile')}`}>
+                <h1 className="login-title">Iniciar sesión</h1>
+                <form className={isDesktop ? 'login-form' : (isTablet ? 'login-form-tablet' : 'login-form-mobile')} method="post" action="/">
 
-                        {/* <input type="text" id="username" name="username" placeholder="Nombre de usuario" required value={formData.username} onChange={handleInputChange}/>
-                        <input type="password" id="password" name="password" placeholder="Contraseña" required value={formData.password} onChange={handleInputChange}/>                        
-                        <button type="submit" onClick={sendForm}>Iniciar sesión</button> */}
+                    <TextField
+                        autoFocus={true}
+                        sx={{ mt: 4, mb: 3, width: '100%', ml: 'auto', mr: 'auto' }}
+                        size='large'
+                        type="text"
+                        id="username"
+                        name="username"
+                        required
+                        label="Nombre de usuario"
+                        value={formData.username}
+                        onChange={handleInputChange}
+                    />
 
-                        <TextField
-                            autoFocus={true}
-                            sx={{ mt: 4, mb: 3, width: '100%', ml: 'auto', mr: 'auto' }}
-                            size='large'
-                            type="text"
-                            id="username"
-                            name="username"
-                            required
-                            label="Nombre de usuario"
-                            value={formData.username}
-                            onChange={handleInputChange}
-                        />
+                    <TextField
+                        sx={{ mt: 2, mb: 3, width: '100%', ml: 'auto', mr: 'auto' }}
+                        size='large'
+                        type={showPassword ? "text" : "password"}
+                        id="password"
+                        name="password"
+                        label="Contraseña"
+                        required
+                        value={formData.password}
+                        onChange={handleInputChange}
+                        InputProps={{
+                            endAdornment: (
+                                <Button onClick={() => setShowPassword(!showPassword)}>
+                                    {showPassword ? <VisibilityOffIcon variant="outlined" /> : <VisibilityIcon variant="filled" />}
+                                </Button>
+                            )
+                        }}
+                    />
 
-                        <TextField
-                            sx={{ mt: 2, mb: 3, width: '100%', ml: 'auto', mr: 'auto' }}
-                            size='large'
-                            type={showPassword ? "text" : "password"}
-                            id="password"
-                            name="password"
-                            label="Contraseña"
-                            required
-                            value={formData.password}
-                            onChange={handleInputChange}
-                            InputProps={{
-                                endAdornment: (
-                                    <Button onClick={() => setShowPassword(!showPassword)}>
-                                        {showPassword ? <VisibilityOffIcon variant="outlined" /> : <VisibilityIcon variant="filled" />}
-                                    </Button>
-                                )
-                            }}
-                        />
+                    <Button
+                        sx={{ mt: 4, mb: 1, width: '100%', ml: 'auto', mr: 'auto' }}
+                        size='small'
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        onClick={sendForm}
+                        disabled={!formData.password || !formData.username}
+                    >
+                        Iniciar sesión
+                    </Button>
 
-                        <Button
-                            sx={{ mt: 4, mb: 1, width: '100%', ml: 'auto', mr: 'auto' }}
-                            size='small'
-                            variant="contained"
-                            color="primary"
-                            type="submit"
-                            onClick={sendForm}
-                            disabled={!formData.password || !formData.username}
-                        >
-                            Iniciar sesión
-                        </Button>
-
-                    </form>
-                    <Box className="user-info">{userInfo.user}</Box>
-                    <Box className="register-link">
-                        <p id="register"><Link to='/register'>Regístrate</Link></p>
-                        <p id="recover"><Link to='/forgot'>Recuperar contraseña</Link></p>
-                    </Box>
-
+                </form>
+                <Box className="user-info">{userInfo.user}</Box>
+                <Box className="register-link">
+                    <p id="register"><Link to='/register'>Regístrate</Link></p>
+                    <p id="recover"><Link to='/forgot'>Recuperar contraseña</Link></p>
                 </Box>
-                {/* <Box className="login-message">{loginData}</Box> */}
-                <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={open} autoHideDuration={!loginData ? 999999 : 5000} onClose={preHandleClose}>
-                    <Alert onClose={preHandleClose} severity="info" sx={{ width: '100%' }}>
-                        {gatheredLoginData}
-                    </Alert>
-                </Snackbar>
+
             </Box>
-        </>
+            {/* <Box className="login-message">{loginData}</Box> */}
+            <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={open} autoHideDuration={!loginData ? 999999 : 5000} onClose={preHandleClose}>
+                <Alert onClose={preHandleClose} severity="info" sx={{ width: '100%' }}>
+                    {gatheredLoginData}
+                </Alert>
+            </Snackbar>
+        </Box>
     )
 }
 
